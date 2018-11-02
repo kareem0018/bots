47f0524db454b2dd39e69b33d5c1872345575b0d
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-11-02 20:33:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+console.log("BOT ONLINE");
+
+const adminprefix = "^";
+    client.on('message', message => {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(adminprefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
+
+client.login(process.env.BOT_TOKEN);
