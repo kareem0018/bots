80118a8468d8aeb8666a7c99090460d3e3afc617
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-11-03 13:01:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,5 @@ client.on('ready', () => {
 
 
 
-client.login('NTA3OTk1NTM4NDI0MDcwMTQ1.Dr4zUQ.H7ANmhi6MmJQiP9THvrxeRo5CXM');
-client.login('NTA3ODA5NjY1MDk5MzY2NDEx.Dr4lfA.cmJlMXt646n-Z0i7MWIWvWbAXWE');
-
+client.login('NDY2MzE4Mzc1NTAxNDMwNzg1.Dr5iEA.WznhsfPzlZn3ULW4fSd3CmzP8pk');
 
