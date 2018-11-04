85b61e09b5b5b7dbe2443f31d63b3b2b3ada5753
eoijfoiwejfoijwfeoijfeoifwej <43916098+oiejoijweojoweijowej@users.noreply.github.com>
2018-11-04 21:46:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send('i love credits')
+        message.channel.send('<@254321678270726144> <@157605500488384512> <@244524477319217152> <@161693885121298432> <@222302020718034944>')
           .then(m => {
             count++;
           })
