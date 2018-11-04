3779b01e900d1d11090cb4e0a68bc4f7be63e6a7
eoijfoiwejfoijwfeoijfeoifwej <43916098+oiejoijweojoweijowej@users.noreply.github.com>
2018-11-04 21:05:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,35 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot1 = new Discord.Client();
 
-console.log("BOT ONLINE");
-
-var prefix = "+";
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`${prefix}say <words>`);
-
-if (command == "say") {
-
-message.channel.send(args.join("  "))
-    message.delete();
-  }
-
-
-
+bot1.on('ready', () => {
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${bot1.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${bot1.guilds.size} " ]`);
+  console.log(`Users! [ " ${bot1.users.size} " ]`);
+  console.log(`channels! [ " ${bot1.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-
-client.on('ready', () => {
-
-	
-
-        client.user.setGame('hi',`http://www.youtube.com/gg`);	
-
-	
-
-});
+ 
 
 
 
-client.login('NDY2MzE4Mzc1NTAxNDMwNzg1.Dr5iEA.WznhsfPzlZn3ULW4fSd3CmzP8pk');
+
+bot1.login("NDY2MzE4Mzc1NTAxNDMwNzg1.Dr5iEA.WznhsfPzlZn3ULW4fSd3CmzP8pk");
+
+
 
